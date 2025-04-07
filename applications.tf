b32a676dacf56540b3d720e6fdc260ba98245b0f
ajay <akn.me007n1.com>
2025-04-07 06:58:39 -0700

    fixing alb target group problems

--- applications.tf
+++ applications.tf
@@ -13,76 +13,31 @@ resource "helm_release" "jenkins" {
     yamlencode({
       persistence = {
         enabled       = true
-        storageClass  = "gp2"
+        storageClass  = "gp2-ebs"
         size          = "7Gi"
-      } 
-   })
+      }
+      controller = {
+        serviceType = "ClusterIP"
+        servicePort = 8080
+        ingress = {
+          enabled = false
+        }
+        jenkinsUriPrefix = "/jenkins"
+        jenkinsUrl = "/jenkins"
+        agentProtocol = ["JNLP4-connect"]
+        resources = {
+          requests = {
+            cpu = "600m"
+            memory = "1Gi"
+          }
+          limits = {
+            cpu = "1"
+            memory = "2Gi"
+          }
+        }
+      }
+    })
   ]
-
-  set {
-    name  = "controller.serviceType"
-    value = "ClusterIP"
-  }
-
-  set {
-    name  = "controller.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/subnets"
-    value = "${replace("subnet-055bdb06e6d448f34,subnet-0d9dece2732432440", ",", "\\,")}"
-  }
-
-  set {
-    name  = "controller.startupProbe.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "controller.ingress.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "service.ports[0].name"
-    value = "http"
-  }
-
-  set {
-    name  = "service.ports[1].name"
-    value = "https"
-  }
-
-  set {
-    name  = "controller.jenkinsUriPrefix"
-    value = "/jenkins"
-  }
-
-  set {
-    name  = "controller.jenkinsUrl"
-    value = "/jenkins"
-  }
-
-  set {
-    name  = "controller.jenkins.agentProtocol"
-    value = "[\"JNLP4-connect\"]"
-  }
-
-  set {
-    name  = "controller.resources.requests.cpu"
-    value = "600m"
-  }
-
-  set {
-    name  = "conroller.resources.requests.memory"
-    value = "1Gi"
-  }
-
-  set {
-    name  = "controller.resources.limits.cpu"
-    value = "1"
-  }
-
-  set {
-    name  = "controller.resources.limits.memory"
-    value = "2Gi"
-  }
 }
 
 resource "helm_release" "argocd" {
@@ -91,54 +46,18 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
-
-  set {
-    name  = "server.service.type"
-    value = "ClusterIP"
-  }
-
-  set {
-    name  = "server.ingress.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "server.extraArgs"
-    value = "{--basehref=/argocd,--insecure}"
-  }
-
-  set {
-    name  = "server.ingress.paths[0]"
-    value = "/argocd"
-  }
-
-  set {
-    name  = "server.ingress.hosts[0]"
-    value = ""
-  }
-  set {
-    name  = "server.ingress.annotations.kubernetes\\.io/ingress\\.class"
-    value = "alb"
-  }
-
-  set {
-    name  = "server.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/scheme"
-    value = "internet-facing"
-  }
-  set {
-    name  = "server.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/load-balancer-attributes"
-    value = "load_balancing.cross_zone.enabled=true"
-  }
-  set {
-    name  = "server.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/target-type"
-    value = "ip"
-  }
-  set {
-    name  = "server.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/group\\.name"
-    value = "shared-alb"
-  }
-  set {
-    name  = "server.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/subnets"
-    value = yamlencode(["subnet-055bdb06e6d448f34", "subnet-0d9dece2732432440"])
-  }
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "ClusterIP"
+          port = 80
+        }
+        ingress = {
+          enabled = false
+        }
+        extraArgs = ["--basehref=/argocd", "--insecure"]
+      }
+    })
+  ]
 }
--- kubernetes_resources.tf
+++ kubernetes_resources.tf
@@ -1,5 +1,9 @@
-
 resource "kubernetes_manifest" "shared_ingress" {
   depends_on = [helm_release.jenkins, helm_release.argocd]
   manifest = yamldecode(file("${path.module}/ingress.yaml"))
+}
+
+resource "kubernetes_manifest" "services" {
+  depends_on = [helm_release.jenkins, helm_release.argocd]
+  manifest = yamldecode(file("${path.module}/services.yaml"))
 }
